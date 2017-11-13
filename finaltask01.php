51449b7d64b5fa46f044a5ef3a7271c6f5fec239
yahiromayu <voyage1122yahiro@outlook.com>
2017-11-13 13:43:17 +0900

    first

---
+++ finaltask01.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>STEP1 最終課題:掲示板を作ろう</title>
+	<?php 
+		//データベース接続
+		if($_SERVER['SERVER_NAME']=="localhost"){
+			$mysqli = new mysqli("localhost","root","","finaltask01");//host,account,passwd,db_name
+		}else{
+			$mysqli = new mysqli("localhost","yahiro","oyasumideshi","yahiro");
+		}
+		if($mysqli->connect_error){
+			print("データベースに接続できません。".$mysqli->connect_error);
+			exit(1);
+		}
+
+		//ブラックリストはここで追加、IPアドレスでチェックを行う、ループバックは::1(なぜかv6)
+		$blacklist = array("");
+	?>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+
+
+<h1>▼掲示板▼</h1>
+<p style="width:80%;margin-left:10%;text-align: center;">*コメント欄の情報は必須です。</p>
+
+
+<!--投稿フォーム-->
+<form method="POST" action="finaltask01.php">
+<ul>
+<li class="in_name">
+	<label for="in_name">名前</label>
+	<input name="name" placeholder="名無しさん">
+</li>
+<li class="in_title">
+	<label for="in_title">タイトル</label>
+	<input name="title" placeholder="">
+</li>
+<li class="in_email">
+	<label for="in_email"> E-MAIL</label>
+	<input name="email">
+</li>
+<li class="in_comment">
+	<label for="in_comment">コメント*</label>
+	<textarea name="comment"></textarea>
+</li>
+<?php
+if(!in_array($_SERVER["REMOTE_ADDR"],$blacklist)){
+?>
+<li>
+	<input id="submit" type="submit" value="送信">
+</li>
+<?php
+}
+?>
+</ul>
+</form>
+
+
+<?php
+//データベース書き込み
+	if(!in_array($_SERVER["REMOTE_ADDR"],$blacklist)){
+		if(isset($_POST["name"],$_POST["comment"],$_POST["title"],$_POST["email"])&&$_POST["comment"]!=""){//名前とコメントがある場合、かつ入力が空でない場合
+			if($_POST["name"]==""){
+				$_POST["name"]="名無しさん";
+			}
+			if($_POST["title"]==""){
+				$_POST["title"]="タイトルなし";
+			}
+			$stmt = $mysqli->prepare("INSERT INTO forum (title,name,email,comment,ipaddr) VALUES (?, ?, ?, ?, ?)");//？部分はあとから代入(プリペアドステートメント)
+			$stmt->bind_param("sssss",$_POST["title"],$_POST["name"],$_POST["email"],$_POST["comment"],$_SERVER['REMOTE_ADDR']);//文字列5つ代入(sはstring、iはint、dはdouble、bはbinary)
+			$stmt->execute();//実行
+		}
+	}else{
+		print("<p style='color:red;text-align:center;'>あなたの使用しているIPアドレスからは書き込みを行えません。</p>");
+	}
+
+//データベース参照、表示
+	$result = $mysqli->query("SELECT * FROM forum ORDER BY time DESC");
+	if($result){
+		while($row = $result->fetch_object()){
+			if(in_array($row->ipaddr,$blacklist))continue;//ブラックリストに入っているipのデータは除く
+			$name = htmlspecialchars($row->name);
+			$title = htmlspecialchars($row->title);
+			$comment = htmlspecialchars($row->comment);
+			$time = htmlspecialchars($row->time);
+			print(nl2br("<div class='box'><div class='title'>▼$title</div><div class='comment'>$comment</div><div class='footer'>( $name | $time )</div></div>"));
+		}
+	}
+
+//データベース接続切断
+	$mysqli->close();
+?>
+
+
+</body>
+</html>
---
+++ finaltask01.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2017 年 11 朁E13 日 05:34
+-- サーバのバージョン： 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `finaltask01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `forum`
+--
+
+CREATE TABLE `forum` (
+  `title` text NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `comment` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ipaddr` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
